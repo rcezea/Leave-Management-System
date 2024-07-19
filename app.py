70302380fbcf0f10fc9cb1edc49ebb731487cb2c
Richard <rcezea007@gmail.com>
2024-07-19 03:37:48 +0100

    production test

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from os import getenv
 from flask import Flask, render_template
 from flask_wtf import csrf
 from flask_cors import CORS
+import dotenv
+dotenv.load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
@@ -24,9 +26,5 @@ def hello_world():
     return render_template('auth/authentication.html')
 
 
-if __name__ == '__main__':
-    from views import app_views
-    app.register_blueprint(app_views)
-    app.run(port=3000,
-            host="0.0.0.0",
-            debug=True)
+from views import app_views
+app.register_blueprint(app_views)
